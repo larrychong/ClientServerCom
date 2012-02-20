0187db82fb6acb5cfd44043e29e456f00ae0d31f
Larry Chong <larry.chong@hotmail.com>
2012-02-20 12:57:56 -0500

    client server communication

---
+++ ServerTest/ServerTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ServerTest
+{
+
+    class Program
+    {
+        static TcpClient connected;
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 3460);
+            listener.Start();
+
+            while (true)
+            {
+                connected = listener.AcceptTcpClient();
+            sendevent:
+                send();
+                goto receiveevent;
+
+            receiveevent:
+                receive();
+                goto sendevent;
+            }
+        }
+        static byte[] buffer = new byte[4096];
+        static void receive()
+        {
+            NetworkStream read = connected.GetStream();
+            int data = read.Read(buffer, 0, 4096);
+            Console.WriteLine("Client: " + Encoding.ASCII.GetString(buffer, 0, data));
+        }
+        static void send()
+        {
+            byte[] data = Encoding.ASCII.GetBytes(Console.ReadLine());//
+            NetworkStream stream = connected.GetStream();
+            stream.Write(data, 0, data.Length);
+        }
+    }
+}
---
+++ client/client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace client
+{
+    class Program
+    {
+        static TcpClient client;
+        static void Main(string[] args)
+        {
+            client = new TcpClient();
+
+            //ipaddress of the Server
+            client.Connect("127.0.0.1", 3460);
+            while (client.Connected)
+            {
+            receivedevent:
+                receive();
+                goto sendevent;
+            sendevent:
+                send();
+                goto receivedevent;
+            }
+        }
+
+        static void send()
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] data = Encoding.ASCII.GetBytes(Console.ReadLine());
+            stream.Write(data, 0, data.Length);
+        }
+
+        static byte[] buffer = new byte[4096];
+        static void receive()
+        {
+            NetworkStream stream = client.GetStream();
+            int data = stream.Read(buffer, 0, 4096);
+            Console.WriteLine("server: " + Encoding.ASCII.GetString(buffer, 0, data));
+        }
+    }
+}
